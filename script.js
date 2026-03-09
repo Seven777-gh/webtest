f1d3537623fdee79d1b50e5d1f55fa9e0defe3c0
Đỗ Tiến Đạt <dotiendat28022007@gmail.com>
2026-03-09 19:49:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function showLogin() {
+
+document.getElementById("app").innerHTML = `
+<div class="center">
+
+<div class="box">
+
+<h2>Login</h2>
+
+<input id="user" placeholder="User"><br>
+<input id="pass" type="password" placeholder="Pass"><br>
+
+<button onclick="login()">Login</button>
+
+<p id="msg"></p>
+
+</div>
+</div>
+`;
+
+}
+
 function login() {
 
 let u = document.getElementById("user").value;
@@ -5,14 +27,46 @@ let p = document.getElementById("pass").value;
 
 if (u == "admin" && p == "123") {
 
-document.body.innerHTML = `
-<h1>Đăng nhập thành công</h1>
-`;
+localStorage.setItem("login", "yes");
+
+showDashboard();
 
 } else {
 
-document.getElementById("msg").innerText = "Sai tài khoản";
+document.getElementById("msg").innerText = "Sai";
 
 }
+
+}
+
+function showDashboard() {
+
+document.getElementById("app").innerHTML = `
+
+<h1>Dashboard</h1>
+
+<button onclick="logout()">Logout</button>
+
+<p>Chào admin</p>
+
+`;
+
+}
+
+function logout() {
+
+localStorage.removeItem("login");
+
+showLogin();
+
+}
+
+if (localStorage.getItem("login") == "yes") {
+
+showDashboard();
+
+} else {
+
+showLogin();
 
 }
